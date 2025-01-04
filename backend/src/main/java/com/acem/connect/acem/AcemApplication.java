b5e90f5e7e10293db1b100a7570ab0c922b108c6
Himanshu Rawat <himanshurawat@192.168.1.15>
2025-01-04 17:18:00 +0530

    chore seup backent & frontend using spring initilizr and vite

---
+++ backend/src/main/java/com/acem/connect/acem/AcemApplication.java
@@ -0,0 +1,13 @@
+package com.acem.connect.acem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/acem/connect/acem/AcemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acem.connect.acem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
